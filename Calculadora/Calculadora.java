c31664c884d3987102fc4e3b4a30bb642b21578f
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-02-25 17:25:19 -0500

    Push de la calculadora

---
+++ Calculadora/Calculadora.java
@@ -0,0 +1,8 @@
+public class Calculadora{
+    public void suma(int a, int b){
+        System.out.println(a+b);
+    }
+    public void suma(int b, int c, int a){
+        System.out.println(a+b+c);
+    }
+}
---
+++ Calculadora/Prueba.java
@@ -0,0 +1,6 @@
+public class Prueba {
+    public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        cal.suma(5,8,7);
+    }
+}
